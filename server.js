2c33333e6a959f8a15125cd6c3064a3df42db7b9
Pablo Perez <pablokris@gmail.com>
2025-05-08 13:12:03 -0700

    business logic updated and now working

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.post('/receipts/process', (req, res) => {
     const id = uuidv4()
     receipts.set(id, receipt)
     res.json({ id })
+    console.log(receipts)
 
   } catch (error) {
     res.status(400).json({ error: 'Please verify input.' })
@@ -33,6 +34,7 @@ app.get('/receipts/:id/points', (req, res) => {
     const { id } = req.params
     const points = calculatePoints(id)
     res.json({ points })
+    console.log(receipts)
   } catch (error) {
     res.status(404).json({ error: 'No receipt found for that ID.' })
   }
@@ -101,32 +103,41 @@ function calculatePoints(id) {
   
   // Rule 1: One point for every alphanumeric character in the retailer name
   points += receipt.retailer.replace(/[^a-zA-Z0-9]/g, '').length
+  console.log(points)
   
   // Rule 2: 50 points if the total is a round dollar amount with no cents
   if (receipt.total.endsWith('.00')) {
     points += 50
+    console.log(points)
   }
   
   // Rule 3: 25 points if the total is a multiple of 0.25
   if (parseFloat(receipt.total) % 0.25 === 0) {
     points += 25
+    console.log(points)
   }
   
   // Rule 4: 5 points for every two items on the receipt
   points += Math.floor(receipt.items.length / 2) * 5
+  console.log(receipt.items.length);
+  console.log('test' + Math.floor(receipt.items.length / 2) * 5)
+  console.log(points)
   
   // Rule 5: If the trimmed length of the item description is a multiple of 3,
   // multiply the price by 0.2 and round up to the nearest integer
   receipt.items.forEach(item => {
     if (item.shortDescription.trim().length % 3 === 0) {
       points += Math.ceil(parseFloat(item.price) * 0.2)
+      console.log(points)
     }
   })
   
   // Rule 6: 6 points if the day in the purchase date is odd
-  const day = new Date(receipt.purchaseDate).getDate()
+  const day = Number(receipt.purchaseDate.split('-')[2]);
   if (day % 2 === 1) {
+    console.log(day + ' is odd 6 points added')
     points += 6
+    console.log(points)
   }
   
   // Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm
@@ -134,7 +145,8 @@ function calculatePoints(id) {
   const purchaseTime = parseInt(hours) + parseInt(minutes) / 60
   if (purchaseTime >= 14 && purchaseTime < 16) {
     points += 10
-  }
+    console.log(points)
+    }
   
   return points
 }
